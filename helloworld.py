9aa84857fb0130c831b6796001abdae935f49af0
Tran Quoc Cuong <cuong.tranquoc@digia.com>
2019-06-03 21:02:02 +0300

    Add format for float value

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 print('Hell yeah')
 print('--------- Format string as an fake table----------')
 print('{0:>10}| {1:<20}| {2:^15}'.format('Id', 'Name', 'Qty'))
-print('{0:>10}| {1:<20}| {2:^15}'.format('01', 'Frist Product', '5'))
+print('{0:>10}| {1:<20}| {2:^15.2f}'.format(
+    '01', 'Frist Product', 5.9878989987979))
 print('{0:>10}| {1:<20}| {2:^15}'.format('02', 'Second Product', '15'))
 print('{0:>10}| {1:<20}| {2:^15}'.format('03', 'Third Product', '8'))
