028ebc37b9ca061fa05c5282ff91e23a6f933160
Tran Quoc Cuong <cuong.tranquoc@digia.com>
2019-06-06 10:34:10 +0300

    clean print monitor, add default parameter for function

--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,27 @@
-print('Hell yeah')
-print('--------- Format string as an fake table----------')
+#print('Hell yeah')
+#print('--------- Format string as an fake table----------')
+"""
 print('{0:>10}| {1:<20}| {2:^15}'.format('Id', 'Name', 'Qty'))
 print('{0:>10}| {1:<20}| {2:^15.2f}'.format(
     '01', 'Frist Product', 5.9878989987979))
 print('{0:>10}| {1:<20}| {2:^15}'.format('02', 'Second Product', '15'))
 print('{0:>10}| {1:<20}| {2:^15}'.format('03', 'Third Product', '8'))
-
-# Read the user's input
+"""
+# --------------Read the user's input--------------
 """
 Here is the way to read user input 
 then print it in terminal
 """
-magentoVersion = input('Currunt magento version that I use is: ')
-print('{} is the current magento version that you use'.format(magentoVersion))
+#magentoVersion = input('Currunt magento version that I use is: ')
+#print('{} is the current magento version that you use'.format(magentoVersion))
 
-# Function defination
-
-
-def showProductName():
-    print('New Marimekko dress')
+# -----------Function defination--------------
 
 
-showProductName()
+def showProductName(productName='default product'):
+    """ Document: this function will return the product name that customer pass to function otherwise it will return default product"""
+    print('New Marimekko dress available in store, here is you product: {} '.format(
+        productName))
+
+
+showProductName('Bim pamper')
